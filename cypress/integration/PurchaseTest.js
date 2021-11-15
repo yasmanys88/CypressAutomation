178b967759391e059e326437da5411361616bf88
Yasmany <yasmany.santana@atos.net>
2021-11-15 15:22:51 -0500

    P3-Login with valid credentials updating

--- cypress/integration/PurchaseTest.js
+++ cypress/integration/PurchaseTest.js
@@ -5,6 +5,10 @@ before(() => {
 });
 describe("Purchase Test", () => {
   it("Purchase some Dress and T-Shirt", () => {
-    loginPage.loginAction("yasmanys88@gmail.com", "d12r57dd");
+    loginPage.loginAction("yasmanys88@gmail.com", "d12r57dd")
+    cy.fixture('homePage').then((home)=>{
+      cy.get(home.homeTitle).should('contain','My account')
+    })
+
   });
 });
