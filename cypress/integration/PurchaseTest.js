c3bc44ba890a785d1e1411f84e3339418ca6ed0e
Yasmany <yasmany.santana@atos.net>
2021-11-15 23:04:45 -0500

    P13-Carry out the procedure of paying for the purchase

--- cypress/integration/PurchaseTest.js
+++ cypress/integration/PurchaseTest.js
@@ -5,45 +5,74 @@ before(() => {
 });
 describe("Purchase Test", () => {
   it("Purchase some Dress and T-Shirt", () => {
-    loginPage.loginAction("yasmanys88@gmail.com", "d12r57dd")
-    cy.fixture('homePage').then((home)=>{
-      cy.get(home.homeTitle).should('contain','My account')
-    })
+    loginPage.loginAction("yasmanys88@gmail.com", "d12r57dd");
 
-    cy.fixture('blockTop').then((searchbox)=>{
-        cy.get(searchbox.searchInput).type('dress')
-        cy.get(searchbox.searchBtn).click()
-    })
+    cy.fixture("homePage").then((home) => {
+      cy.get(home.homeTitle).should("contain", "My account");
+    });
 
-    cy.fixture('search').then((search)=>{
-        cy.get(search.pageTitle).should('contain','dress')
-        cy.get(search.firtsResult).should('contain','In stock')
-        cy.get(search.addToCard).click()
-    })
-    cy.fixture('layerCart').then((layercart)=>{
-        cy.get(layercart.titleCartProduct).should('contain','Product successfully added to your shopping cart')
-        cy.get(layercart.continueBtn).click()
-    })
-    cy.fixture('blockTop').then((blocktop)=>{
-        cy.get(blocktop.cart_quantity).should('contain','1')
-    })
-    cy.fixture('categories').then((categories)=>{
-      cy.get(categories.tshirtlink).click()
-    })
-    cy.fixture('tshirts').then((tshirt)=>{
-      cy.get(tshirt.title).should('contain','T-shirts')
-      cy.get(tshirt.available).should('contain','In stock')
-      cy.get(tshirt.moreBtn).click()
-    })
-    cy.fixture('product').then((products)=>{
-      cy.get(products.plusBtn).click()
+    cy.fixture("blockTop").then((searchbox) => {
+      cy.get(searchbox.searchInput).type("dress");
+      cy.get(searchbox.searchBtn).click();
+    });
+
+    cy.fixture("search").then((search) => {
+      cy.get(search.pageTitle).should("contain", "dress");
+      cy.get(search.firtsResult).should("contain", "In stock");
+      cy.get(search.addToCard).click();
+    });
+
+    cy.fixture("layerCart").then((layercart) => {
+      cy.get(layercart.titleCartProduct).should(
+        "contain",
+        "Product successfully added to your shopping cart"
+      );
+      cy.get(layercart.continueBtn).click();
+    });
+
+    cy.fixture("blockTop").then((blocktop) => {
+      cy.get(blocktop.cart_quantity).should("contain", "1");
+    });
+
+    cy.fixture("categories").then((categories) => {
+      cy.get(categories.tshirtlink).click();
+    });
+
+    cy.fixture("tshirts").then((tshirt) => {
+      cy.get(tshirt.title).should("contain", "T-shirts");
+      cy.get(tshirt.available).should("contain", "In stock");
+      cy.get(tshirt.moreBtn).click();
+    });
+    cy.fixture("product").then((products) => {
+      cy.get(products.plusBtn).click();
       //cy.get(products.plusBtn).type('2') /* Another way  */
-      cy.get(products.addtocartBtn).click()
-    })
-    cy.fixture('layerCart').then((layercart)=>{
-      cy.get(layercart.titleCartProduct).should('contain','Product successfully added to your shopping cart')
-      cy.get(layercart.checkoutBtn).click()
-  })
+      cy.get(products.addtocartBtn).click();
+    });
+
+    cy.fixture("layerCart").then((layercart) => {
+      cy.get(layercart.titleCartProduct).should(
+        "contain",
+        "Product successfully added to your shopping cart"
+      );
+      cy.get(layercart.checkoutBtn).click();
+    });
+
+    cy.fixture("shoppinCar").then((sc) => {
+      cy.get(sc.checkoutBtn).click();
+      cy.get(sc.checkoutBtn).click();
+      cy.get(sc.checkboxShiping).check();
+      cy.get(sc.checkoutBtn).click();
+      cy.get(sc.paybyCheck).click();
+      cy.get(sc.checkoutBtn).click();
+      cy.get(sc.alerta).should(
+        "contain",
+        "Your order on My Store is complete."
+      );
+      cy.get(sc.backtoordersBtn).click();
+    });
+
+    
+
 
   });
 });
